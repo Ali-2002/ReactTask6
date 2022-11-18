cf2072932b3849648be216d0fba0ff14bc953bdf
Faxralinski <turanisback@gmail.com>
2022-11-18 19:05:31 +0400

    finish

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hospital from './components/Hospital/Hospital'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Hospital/>
+    </div>
+  )
+}
+
+export default App
